e35fe718b4a34900bbe7e83716fe3b5bb3cc0098
madnezz <niclas.persson01@gmail.com>
2023-07-03 23:55:53 +0200

    Simple maze constructed, Collision bug resolved. 0.1.2

--- PacMan2.py
+++ PacMan2.py
@@ -3,8 +3,21 @@ import sys
 
 WIDTH = 1280
 HEIGHT = 720
-BACKGROUND = (0, 0, 0)
-VERSION = 0.1
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+VERSION = "0.1.2"
+
+maze_data = [
+    ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", ".", ".", ".", ".", ".", "#", ".", ".", ".", ".", ".", ".", ".", ".", "#"],
+    ["#", "#", "#", "#", "#", ".", "#", ".", "#", "#", "#", "#", ".", "#", ".", "#"],
+    ["#", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "#", ".", "#", ".", "#"],
+    ["#", ".", "#", "#", "#", "#", "#", ".", "#", ".", "#", "#", ".", "#", ".", "#"],
+    ["#", ".", ".", ".", ".", ".", ".", ".", "#", ".", ".", ".", ".", ".", ".", "#"],
+    ["#", ".", ".", ".", ".", ".", ".", ".", "#", ".", ".", ".", ".", ".", ".", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"]
+]
 
 
 class Sprite(pygame.sprite.Sprite):  # Super Class made by pygame
@@ -52,6 +65,7 @@ class Player(Sprite):  # Player class, representing PacMan
         key = pygame.key.get_pressed()  # Returns pressed keys
         # Collision detection, should probably be separate function
         collision = pygame.sprite.spritecollideany(self, walls)
+
         if collision and self.direction == "left":
             self.left = False
             self.move(self.speed, 0)  # Bounce back slightly (less likely to get stuck)
@@ -60,16 +74,18 @@ class Player(Sprite):  # Player class, representing PacMan
             self.move(-self.speed, 0)  # Bounce back slightly
         if collision and self.direction == "up":
             self.up = False
-            self.move(0, -self.speed)  # Bounce back slightly
+            self.move(0, self.speed)  # Bounce back slightly
         if collision and self.direction == "down":
             self.down = False
-            self.move(0, self.speed)  # Bounce back slightly
+            self.move(0, -self.speed)  # Bounce back slightly
 
         if key[pygame.K_LEFT] and self.left == True:  # What to do if left arrow is pressed
             self.move(-self.speed, 0)  # Move player
             self.move_left_img()  # Change image of player
             self.direction = "left"  # Store direction, used for collision
             self.right = True  # Reverts previous collision block
+            self.down = True
+            self.up = True
             if not pygame.mixer_music.get_busy():
                 pygame.mixer_music.load("sound/pacman_chomp.wav")
                 pygame.mixer_music.play(1)
@@ -79,6 +95,8 @@ class Player(Sprite):  # Player class, representing PacMan
             self.move_right_img()
             self.direction = "right"
             self.left = True
+            self.down = True
+            self.up = True
             if not pygame.mixer_music.get_busy():
                 pygame.mixer_music.load("sound/pacman_chomp.wav")
                 pygame.mixer_music.play(1)
@@ -88,6 +106,8 @@ class Player(Sprite):  # Player class, representing PacMan
             self.move_up_img()
             self.direction = "up"
             self.down = True
+            self.left = True
+            self.right = True
             if not pygame.mixer_music.get_busy():
                 pygame.mixer_music.load("sound/pacman_chomp.wav")
                 pygame.mixer_music.play(1)
@@ -97,19 +117,32 @@ class Player(Sprite):  # Player class, representing PacMan
             self.move_down_img()
             self.direction = "down"
             self.up = True
+            self.left = True
+            self.right = True
             if not pygame.mixer_music.get_busy():
                 pygame.mixer_music.load("sound/pacman_chomp.wav")
                 pygame.mixer_music.play(1)
 
 
-class Wall(Sprite):  # Block class, made to build the maze
-    def __init__(self, startx, starty):
-        super().__init__("images/bricks.png", startx, starty)
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill((0, 0, 255))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 
-class Ghost(Sprite):  # Ghost class
-    def __init__(self, startx, starty, image):
-        super().__init__(image, startx, starty)
+class Point(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        super().__init__()
+        self.radius = radius
+        self.image = pygame.Surface([radius * 2, radius * 2], pygame.SRCALPHA)
+        self.rect = self.image.get_rect(center=(x, y))
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, YELLOW, self.rect.center, self.radius)
 
 
 def main():
@@ -120,24 +153,39 @@ def main():
     pygame.mixer_music.play(1)
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(200, 200)
+    player = Player(300, 300)
+
+    cell_width = WIDTH // len(maze_data[0])
+    cell_height = (HEIGHT // len(maze_data)) - 10  # This makes no sense but is needed (Top bar takes space?)
+    dot_radius = min(cell_width, cell_height) // 8
+    print(dot_radius)
 
     walls = pygame.sprite.Group()  # Create a group since we will create a LOT of wall-segments
-    for wls in range(0, WIDTH, 28):  # Upper and lower wall
-        walls.add(Wall(wls, HEIGHT - 20))  # Ignore warning
-        walls.add(Wall(wls, 20))
-    for wls in range(0, HEIGHT, 28):  # Western and eastern wall
-        walls.add(Wall(20, wls))  # Ignore warning
-        walls.add(Wall(WIDTH - 20, wls))
+    points = pygame.sprite.Group()  # Create a group of points
+
+    for row in range(len(maze_data)):
+        for col in range(len(maze_data[row])):
+            if maze_data[row][col] == "#":
+                x = col * cell_width
+                y = row * cell_height
+                wall = Wall(x, y, cell_height, cell_width)
+                walls.add(wall)
+            elif maze_data[row][col] == ".":
+                x = col * cell_width + cell_height // 2
+                y = row * cell_width + cell_height // 2
+                dot = Point(x, y, dot_radius)
+                points.add(dot)
 
     while True:
-        screen.fill(BACKGROUND)
-        pygame.event.pump()
+        screen.fill(BLACK) # Make screen Black
+        pygame.event.pump() # internally process pygame event handlers
         player.update(walls)
-        player.draw(screen)
-        walls.draw(screen)
-        pygame.display.flip()
-        clock.tick(60)
+        player.draw(screen) # Draw player
+        walls.draw(screen) # Draw walls
+        for dot in points: # Draw points
+            dot.draw(screen)
+        pygame.display.flip() # Update the full display Surface to the screen
+        clock.tick(60) # Limits FPS (affects game speed aswell)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # if "X" is pressed on window, close application

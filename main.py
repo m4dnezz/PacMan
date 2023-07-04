28d04fce89d633ab7bc32aa7cc5d5896384f34c7
madnezz <niclas.persson01@gmail.com>
2023-07-04 17:43:07 +0200

    Created main.py

--- main.py
+++ main.py
@@ -12,11 +12,6 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"]
 ]
 
-Game_Running = True
-WIDTH = 1280
-HEIGHT = 720
-VERSION = "0.1.5"
-
 if __name__ == "__main__":
     pygame.init()
     pygame.mixer.init()
